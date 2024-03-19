e113dde26cf16f2e9812ae25e15d59c1c6f12979
Peter Snelgrove <peter_snelgrove@rapid7.com>
2024-03-19 09:57:42 -0400

    add missing lua module for wezterm

---
+++ .config/wezterm/utils/strings.lua
@@ -0,0 +1,15 @@
+
+local strings = {}
+
+function strings.split(s, sep)
+  if sep == nil then
+    sep = "%s"
+  end
+  local t={}
+  for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+    table.insert(t, str)
+  end
+  return t
+end
+
+return strings
